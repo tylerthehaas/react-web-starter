793e2c20c637fd36a8e7547df78a1761ba72c628
Tyler Haas <haasdevelopment@gmail.com>
2018-01-14 16:34:02 -0700

    Finished out todo app

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Item from './Item'
 
 export class Todo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [],
+      inputText: '',
+    }
+  }
+
+  addTodoHandler = () => {
+    const { inputText } = this.state
+    if (!inputText) return
+    this.state.todos.push({ id: Date.now(), text: inputText })
+  }
+
+  inputTextHandler = text => (this.state.inputText = text)
+
   render() {
+    const { todos } = this.state
     return (
-      <div>hello</div>
-    );
+      <div>
+        <div class="todo-list">
+          {todos.map(v => <Item key={v.id} text={v.text} />)}
+        </div>
+        <AddTodo inputHandler={inputTextHandler} btnHandler={addTodoHandler} />
+      </div>
+    )
   }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Todo } from './Todo';
+import React from 'react'
+import { render } from 'react-dom'
+import { Todo } from './Todo'
 
-render(<Todo />, document.getElementById('app'));
+render(<Todo />, document.getElementById('app'))
